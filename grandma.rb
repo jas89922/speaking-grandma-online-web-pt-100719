0a8bd3cab2a0823061b9f81465b9e419bdba6e44
Jasmine Kim <jas89922@gmail.com>
2019-10-14 05:31:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,5 @@
 def speak_to_grandma
   if "I LOVE YOU GRANDMA!" == "I LOVE YOU TOO!"
   else "Hi Nana, how are you?" == "HUH?! SPEAK UP, SONNY!"
-    if "WHAT DID YOU EAT TODAY?" == "NO, NOT SINCE 1938!"
-      if "WHAT?" == "NO, NOT SINCE 1938!"
+  elsif "WHAT?" == "NO, NOT SINCE 1938!"
      end
