1dacf1341c8a17c789e62b82616cceba409535cb
Jasmine Kim <jas89922@gmail.com>
2019-10-14 05:32:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
-def speak_to_grandma
-  if "I LOVE YOU GRANDMA!" == "I LOVE YOU TOO!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!" == "I LOVE YOU TOO!"
   else "Hi Nana, how are you?" == "HUH?! SPEAK UP, SONNY!"
   elsif "WHAT?" == "NO, NOT SINCE 1938!"
      end
